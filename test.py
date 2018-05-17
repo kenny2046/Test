72b7c5275be1423f6c67ca6748137871af3a08ec
Boyang <545585915@qq.com>
2018-05-17 10:52:04 -0400

    commit 2

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import pymongo
 
 print("Welcom e to Python")
-
+print("Welcom e to Python1")
+print("Welcom e to Python2")
 def update_vm()
         try:
             if a:
+ffff
+
+
+
+ddddd
+
+
+delattr()
